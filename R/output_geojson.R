0a25e9f15ff74507eb4ebc013174aee3cd45c847
Andy Teucher <andy.teucher@gov.bc.ca>
2014-10-22 15:32:43 -0700

    Build on ropensci::to_geojson

--- R/output_geojson.R
+++ R/output_geojson.R
@@ -3,22 +3,30 @@
 #' Deals with a peculiarity of writeOGR where file has to be \code{".geojson"}, optionally preceeded by a path, but cannot have a filename preceeding the \code{".geojson"}.
 #' Also deals with factors (including ordered factors)
 #' @import rgdal
-#' @param spdf The SpatialDataFrame
-#' @param  outpath The output path
-#' @param  outname The output filename
+#' @param input The SpatialDataFrame
+#' @param  destpath The output path
+#' @param  outfilename The output filename
 #' @export
 #' @return Nothing (writes file to specified location)
 #' @examples \dontrun{
 #' output_geojson(myspatialdataframe, "out", "file.geojson")
 #'
 #'}
-output_geojson <- function (spdf, outpath, outname) {
-  
-  if(any(missing(spdf), missing(outpath), missing(outname))) {
-    stop("You must provide all arguments")
+output_geojson <- function(input, destpath = "~/", outfilename = "myfile"){
+  if (!grepl("\\.geojson$",outfilename)) {
+    outfilename <- paste0(outfilename, ".geojson")
   }
   
-  spdf@data <- data.frame(lapply(spdf@data, function(x) {
+  destpath <- path.expand(destpath)
+  
+  basefilename <- strsplit(outfilename,"\\.")[[1]][1]
+  
+  unlink(c(file.path(destpath, outfilename), 
+           file.path(destpath, basefilename)))
+  
+  if (!file.exists(destpath)) dir.create(destpath)
+  
+  input@data <- data.frame(lapply(input@data, function(x) {
     if ("factor" %in% class(x)) {
       x <- as.character(x)
       message("Column is a factor, converting to character")
@@ -26,8 +34,13 @@ output_geojson <- function (spdf, outpath, outname) {
     x
   }), stringsAsFactors = FALSE)
   
-  dsn <- paste0(outpath, "/.geojson")
+  dsn <- file.path(destpath, basefilename)
+  writeOGR(input, dsn = dsn, layer = "", driver = "GeoJSON")
   
-  writeOGR(spdf, dsn = dsn, layer = "", driver = "GeoJSON")
-  system(paste0("mv ", dsn, " ", outpath, "/", outname))
+  dest <- file.path(destpath, outfilename)
+  file.rename(dsn, dest)
+  
+  message("Success! File is at ", dest)
 }
+
+## Try to integrate it with https://github.com/ropensci/togeojson/blob/master/R/to_geojson.r and do a PR there
