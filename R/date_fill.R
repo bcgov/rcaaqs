177227f724279c2c71a9b99430f102ff394d394e
Andy Teucher <andy.teucher@gov.bc.ca>
2014-10-28 10:27:39 -0700

    Put find_time_int into date_fill.R

--- R/date_fill.R
+++ R/date_fill.R
@@ -50,3 +50,27 @@ date_fill <- function (df, date_col, interval = NULL, fill_cols = NULL) {
   
   out
 }
+
+#'Find time interval in a date sequence
+#'
+#'Taken from package openair
+#' @param dates vector of dates
+#' @export
+#' @return an integer reflecting the number of seconds in the time interval
+#' @examples \dontrun{
+#'
+#'}
+find_time_int <- function (dates) {
+  dates <- unique(dates)
+  len <- length(dates)
+  len <- min(c(100, len))
+  id <- which.max(table(diff(as.numeric(dates[order(dates[1:len])]))))
+  seconds <- as.numeric(names(id))
+  if ("POSIXt" %in% class(dates)) 
+    seconds <- paste(seconds, "sec")
+  if (class(dates)[1] == "Date") {
+    seconds <- 3600 * 24
+    seconds <- paste(seconds, "sec")
+  }
+  seconds
+}
