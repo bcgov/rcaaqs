fb559bc93289e9ff4b14e1de607cbd65cb202cbf
Andy Teucher <andy.teucher@gov.bc.ca>
2014-12-04 16:30:56 -0800

    Add grouping variables to pm_avg_daily

--- R/pm_avg_daily.R
+++ R/pm_avg_daily.R
@@ -1,27 +1,32 @@
 #' Calcualte the daily average PM2.5 reading, following CAAQS data completeness rules
 #'
 #' @param data data frame with date and value
-#' @param  datecol the name (as a character string) of the date column
-#' @param  valcol the name (as a character string) of the PM2.5 value column
+#' @param datecol the name (as a character string) of the date column
+#' @param valcol the name (as a character string) of the PM2.5 value column
+#' @param ... grouping variables, probably an id if using multiple sites
 #' @import dplyr
 #' @import lazyeval
 #' @importFrom lubridate year
 #' @export
-#' @return data frame with the daily averages
+#' @return data frame with the daily averages, can be input into \code{\link{pm_98_percentile}}
+#' @seealso \code{\link{pm_98_percentile}}
 #' @examples \dontrun{
 #'
 #'}
-pm_avg_daily <- function(data, datecol, valcol) {
+pm_avg_daily <- function(data, datecol, valcol, ...) {
   ## if datecol is a datetime column, convert to date
   data$date <- as.Date(data[[datecol]])
   
-  ## capture the formulas for the summaries
+  # Capture grouping variables
+  dots <- list(..., "date")
+  
+  ## Capture the formulas for the summaries
   readings_formula <- interp(~length(na.omit(x)), x = as.name(valcol))
   avg_formula <- interp(~ifelse(n_readings >= 18, mean(x, na.rm = TRUE),
                                 NA_real_),
                         x = as.name(valcol))
   
-  group_by(data, date) %>%
+  group_by_(data, .dots = dots) %>%
     summarise_(n_readings = readings_formula,
                avg_24hr = avg_formula) %>%
     mutate(year = lubridate::year(date))
