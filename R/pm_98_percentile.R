44549815b7222e518b26a5be6c6acfd3e2eec20c
Andy Teucher <andy.teucher@gov.bc.ca>
2014-12-10 11:21:34 -0800

    percent_valid_days function

--- R/pm_98_percentile.R
+++ R/pm_98_percentile.R
@@ -13,10 +13,12 @@
 #' @examples \dontrun{
 #' 
 #'}
-pm_98_percentile <- function(data, yearcol, valcol, ...) {
+pm_98_percentile <- function(data, datecol, valcol, ...) {
   data <- data[!is.na(data[[valcol]]),]
   
-  dots <- list(..., yearcol)
+  data$year <- as.POSIXlt(data[[datecol]])$year + 1900
+  
+  dots <- list(..., "year")
   
   arrange_formula <- interp(~desc(x), x = as.name(valcol))
   
@@ -27,9 +29,58 @@ pm_98_percentile <- function(data, yearcol, valcol, ...) {
   }
   
   ans <- group_by_(data, .dots = dots) %>%
+    mutate_(n_days = ~n(),
+      percent_valid_annual = , 
+      percent_valid_q1 = , 
+      percent_valid_q2 = , 
+      percent_valid_q3 = , 
+      percent_valid_q4 = ) %>%
     arrange_(arrange_formula) %>%
     slice(cut_rank(n()))
   
   ans
   
 }
+
+
+#' Given a vector of dates (in a single year), calculate the percentage of days in a quarter
+#' 
+#' @param  date a vecvtor of dates
+#' @param  tz the timezone the dates are in. Default Etc/GMT-8 with no Daylight savings
+#' @param  q the quarter of interest
+#' @export
+#' @return  A percentage of days in the specified quarter that are in the supplied vector
+#' @examples \dontrun{
+#' 
+#'}
+percent_valid_days <- function(dates, tz = "Etc/GMT-8", q = c("year","Q1","Q2","Q3","Q4")) {
+  q = match.arg(q)
+  
+  dates <- na.omit(dates)
+  
+  year <- as.POSIXlt(dates[1])$year + 1900
+  
+  q_lengths <- c(year = -difftime(paste0(year, "-01-01"), paste0(year, "-12-31"), 
+                                  tz = tz, units = "days"), 
+                 Q1 = -difftime(paste0(year, "-01-01"), paste0(year, "-03-31"), 
+                                tz = tz, units = "days"), 
+                 Q2 = -difftime(paste0(year, "-04-01"), paste0(year, "-06-30"), 
+                                tz = tz, units = "days"), 
+                 Q3 = -difftime(paste0(year, "-07-01"), paste0(year, "-09-30"), 
+                                tz = tz, units = "days"), 
+                 Q4 = -difftime(paste0(year, "-10-01"), paste0(year, "-12-31"), 
+                                tz = tz, units = "days")) + 1
+  
+  q_length = q_lengths[q]
+  
+  if (q == "year") {
+    q_dates <- dates
+  } else {
+    q_dates <- dates[quarters(dates) %in% q]
+  }
+  
+  ret <- (length(q_dates) / q_length) * 100
+  
+  ret
+}
+
