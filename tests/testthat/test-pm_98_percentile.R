74ff247897c9d19af8be1546f70690dd88137be7
Andy Teucher <andy.teucher@gov.bc.ca>
2014-12-11 11:38:48 -0800

    testing pm_98_percentile

--- tests/testthat/test-pm_98_percentile.R
+++ tests/testthat/test-pm_98_percentile.R
@@ -1,10 +1,10 @@
-library("bcairquality")
-
 context("Percent valid days")
 
+dates <- seq.Date(from = as.Date("2011-01-01"), to = as.Date("2011-12-31"), by = "days")
+ly_dates <- seq.Date(from = as.Date("2012-01-01"), to = as.Date("2012-12-31"), by = "days")
+
 test_that("Works with a full time series", {
-  dates <- seq.Date(from = as.Date("2011-01-01"), to = as.Date("2011-12-31"), by = "days")
-  
+    
   expect_equal(percent_valid_days(dates, q = "year"), c(year = 100))
   expect_equal(percent_valid_days(dates, q = "Q1"), c(Q1 = 100))
   expect_equal(percent_valid_days(dates, q = "Q2"), c(Q2 = 100))
@@ -12,8 +12,6 @@ test_that("Works with a full time series", {
   expect_equal(percent_valid_days(dates, q = "Q4"), c(Q4 = 100))
   
   # And in a leap year
-  dates <- seq.Date(from = as.Date("2012-01-01"), to = as.Date("2012-12-31"), by = "days")
-  
   expect_equal(percent_valid_days(dates, q = "year"), c(year = 100))
   expect_equal(percent_valid_days(dates, q = "Q1"), c(Q1 = 100))
   expect_equal(percent_valid_days(dates, q = "Q2"), c(Q2 = 100))
@@ -96,4 +94,22 @@ test_that("Duplicated dates cause a warning", {
   dates <- c(dates, dates)
   
   expect_warning(percent_valid_days(dates, q = "year"))
+})
+
+context("pm 98 percentile")
+
+one_year <- data.frame(dates, val = rnorm(length(dates), 20,5))
+
+test_that("Is a data frame", {
+  expect_is(pm_98_percentile(one_year, datecol = "dates", valcol = "val"), "data.frame")
+})
+
+test_that("Has the right column names and dimensions", {
+  test <- pm_98_percentile(one_year, datecol = "dates", valcol = "val")
+  expected_names <- c("year", "n_days", "percent_valid_annual", 
+                      "percent_valid_q1", "percent_valid_q2", 
+                      "percent_valid_q3", "percent_valid_q4", 
+                      "ann_98_percentile")
+  expect_equal(names(test), expected_names)
+  expect_equal(dim(test), c(1, 8))
 })
