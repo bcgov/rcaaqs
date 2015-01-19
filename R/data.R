bad580da03e6be0fb8cf2443b379b10a6aa6ff1b
Andy Teucher <andy.teucher@gov.bc.ca>
2015-01-19 14:13:44 -0800

    rename airzones -> airzone_map

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Borders of British Columbia Regional Districts
+#'
+#' @format A SpatialPolgonsDataFrame with 29 polygons and 2 variables:
+#' \describe{
+#' \item{unit_name}{The name of the region}
+#' \item{region_type}{The type of region (Regional District, Regional Municipality, or Unincorporated Area)}
+#' }
+"rd_map"
+
+#' Borders of British Columbia Airzones
+#'
+#' @format A SpatialPolgonsDataFrame with 7 polygons and 1 variable
+#' \describe{
+#' \item{Airzone}{The name of the airzone}
+#' }
+"airzone_map"
--- data-raw/airzones.R
+++ data-raw/airzones.R
@@ -5,8 +5,18 @@ bc_albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000
 
 unzip("data-raw/airzones.zip", exdir = "data-raw")
 
-airzones <- readOGR(dsn = "data-raw/airzones", layer = "fixed-Polygon", stringsAsFactors = FALSE)
+airzone_map <- readOGR(dsn = "data-raw/airzones", layer = "fixed-Polygon", stringsAsFactors = FALSE)
 
-airzones <- spTransform(airzones, CRSobj = CRS(bc_albers))
+airzone_name_lu <- data.frame(FID = c(0,1,2,3,4,5,6), 
+                              Airzone = c("Northeast", "Northwest", 
+                                          "Southern Interior", 
+                                          "Lower Fraser Valley", 
+                                          "Central Interior", 
+                                          "Coastal", "Georgia Strait"), 
+                              stringsAsFactors = FALSE)
 
-devtools::use_data(airzones, pkg = devtools::as.package("."), overwrite = TRUE, compress = "xz")
+airzone_map <- merge(airzone_map, airzone_name_lu, by = "FID")
+
+airzone_map <- spTransform(airzone_map, CRSobj = CRS(bc_albers))
+
+devtools::use_data(airzone_map, pkg = devtools::as.package("."), overwrite = TRUE, compress = "xz")
