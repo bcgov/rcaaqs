d964d0efec38a1cca2b4c2d4d43ee29d7fc5a0ef
Andy Teucher <andy.teucher@gov.bc.ca>
2015-01-19 12:46:32 -0800

    Revert "Move common calls to maps-raw.R"
    This reverts commit eaf50bf995e04ff18e35669e89ca35a55c8bb78b.

--- data-raw/airzones.R
+++ data-raw/airzones.R
@@ -1,3 +1,10 @@
+library("sp")
+library("rgdal")
+
+bc_albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
+unzip("data-raw/airzones.zip", exdir = "data-raw")
+
 airzones <- readOGR(dsn = "data-raw/airzones", layer = "fixed-Polygon", stringsAsFactors = FALSE)
 
 airzones <- spTransform(airzones, CRSobj = CRS(bc_albers))
--- data-raw/maps-raw.R
+++
@@ -1,9 +0,0 @@
-library("sp")
-library("rgdal")
-
-bc_albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-nad_83 <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
-
-unzip("data-raw/airzones.zip", exdir = "data-raw")
-
-unzip("data-raw/regional_districts_ceei.zip", exdir = "data-raw/rd")
