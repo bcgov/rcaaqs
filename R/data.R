763cd607b06fa397442ea18bced224f969eab4fd
Andy Teucher <andy.teucher@gov.bc.ca>
2015-08-18 13:06:46 -0700

    Remove raw data and RDs

--- R/data.R
+++ R/data.R
@@ -10,21 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#' Borders of British Columbia Regional Districts
-#' 
-#' Borders were taken from the Community Energy Emissions Inventory shapefile 
-#' available from the source below. Northern Rockies Regional Municipality was 
-#' added manually, as it was not available in the original source file.
-#' 
-#' @format A SpatialPolgonsDataFrame with 29 polygons and 2 variables: 
-#'   \describe{ \item{region_name}{The name of the region} 
-#'   \item{region_type}{The type of region (Regional District, Regional
-#'   Municipality, or Unincorporated Area)} }
-#' @source
-#'   \url{http://catalogue.data.gov.bc.ca/dataset/ceei-primary-indicators-total-2007-regional-districts}
-#'   
-"rd_map"
-
 #' Borders of British Columbia Airzones
 #'
 #' @format A SpatialPolgonsDataFrame with 7 polygons and 1 variable
