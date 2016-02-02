141fbb01458fc15983d07de18dc8fb24c9827796
Andy Teucher <andy.teucher@gov.bc.ca>
2016-02-01 16:50:18 -0800

    Tweak docs

--- R/dates.R
+++ R/dates.R
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#'Convert character date string from Raw air data to POSIXct
+#'Convert character date string from air data to POSIXct
 #'
 #' Likely data downloaded from DataBC
 #' @importFrom lubridate fast_strptime
