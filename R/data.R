f81931de37bc73e683e427e268a8a5dc02d4ddba
aman <aman.verma.mtl@gmail.com>
2016-12-08 17:57:57 -0500

    updating ozone data and man page

--- R/data.R
+++ R/data.R
@@ -65,3 +65,18 @@
 #'   \item{value}{PM2.5 hourly value (in ppb)}
 #' }
 "pm25_sample_data"
+
+#' Sample hourly ozone data for 10 stations
+#'
+#' @format A data frame with 6 variables:
+#' \describe{
+#'   \item{ems_id}{id of the monitoring station}
+#'   \item{date_time}{date/time (POSIXct) in ETC/GMT+8 timezone}
+#'   \item{site}{name of the monitoring station}
+#'   \item{year}{year}
+#'   \item{month}{month}
+#'   \item{parameter}{name of the parameter ("O3")}
+#'   \item{value}{ozone hourly value}
+#'   \item{units}{the units of value -- always 'ppb'}
+#' }
+"o3_sample_data"
