de9c2b5ad11713de54671d984c869254b1f8d6c2
aman <aman.verma.mtl@gmail.com>
2016-12-08 12:51:04 -0500

    Restored and modified tests for percentile

--- tests/testthat/test-pm_98_percentile.R
+++ tests/testthat/test-pm_98_percentile.R
@@ -62,63 +62,67 @@ test_that("passes correctly to quantile", {
 })
 
 
+context("pm 98 percentile")
 
-# context("pm 98 percentile")
-# 
-# multi_id <- readRDS("daily_averages.rds")
-# 
-# one_id <- multi_id[multi_id$id == "a", ]
-# 
-# # Make a multi-year df
-# # dates2 <- seq.Date(from = as.Date("2012-01-01"),
-# #                             to = as.Date("2012-12-31"), by = "days")
-# # mult_years <- data.frame(id = c(rep("a", length(dates)), rep("b", length(dates2))),
-# #                          dates = c(dates, dates2),
-# #                          val = rnorm(length(dates) + length(dates2), 20, 5),
-# #                          stringsAsFactors = FALSE)
-# 
+multi_id <- readRDS("daily_averages.rds")
+one_id <- multi_id[multi_id$id == "a", ]
+
+# Make a multi-year df
+# dates2 <- seq.Date(from = as.Date("2012-01-01"),
+#                             to = as.Date("2012-12-31"), by = "days")
+# mult_years <- data.frame(id = c(rep("a", length(dates)), rep("b", length(dates2))),
+#                          dates = c(dates, dates2),
+#                          val = rnorm(length(dates) + length(dates2), 20, 5),
+#                          stringsAsFactors = FALSE)
+
+test_one <- pm_yearly_98(one_id, dt = "dates", val = "val")
+test_mult <- pm_yearly_98(multi_id, dt = "dates", val = "val", by = "id")
+  
 # test_one <- pm_98_percentile(one_id, date = "dates", val = "val")
 # test_mult <- pm_98_percentile(multi_id, date = "dates", val = "val", by = "id")
-# 
-# test_that("Only accepts date-type objects with time interval of one day", {
-#   posix <- seq.POSIXt(as.POSIXct("2012-01-01"), as.POSIXct("2012-01-31"), by = "hour")
-#   test <- data.frame(posix, val = rnorm(length(posix), 20, 5))
-# 
-#   expect_error(pm_98_percentile(test, "posix", "val"))
-# })
-# 
-# test_that("Is a data frame", {
-#   expect_is(test_one, "data.frame")
-#   expect_is(test_mult, "data.frame")
-# })
-# 
-# test_that("Has the right column names and dimensions", {
-#   expected_names <- c("year", "n_days", "ann_98_percentile", "exceed")
-#   expect_equal(names(test_one), expected_names)
-#   expect_equal(dim(test_one), c(3, 4))
-# 
-#   # For multiple years:
-#   expect_equal(names(test_mult), c("id", expected_names))
-#   expect_equal(dim(test_mult), c(6, 5))
-# })
-# 
-# test_that("Columns are the right class", {
-#   classes <- c("integer", "integer", "numeric", "logical")
-#   expect_equal(unname(sapply(test_one, class)), classes)
-#   expect_equal(unname(sapply(test_mult, class)), c("character", classes))
-# })
-# 
-# test_that("Exceed works", {
-#   expect_false(any(test_mult$exceed))
-# 
-#   set.seed(42)
-#   multi_id$val <- rnorm(nrow(multi_id), 35, 1)
-#   res <- pm_98_percentile(multi_id, date = "dates", val = "val", by = "id",
-#                           std = 28)
-#   expect_true(all(res$exceed))
-# })
-# 
-# test_that("Number of days is correct", {
-#   expect_equal(test_one$n_days, c(317L, 332L, 317L))
-#   expect_equal(test_mult$n_days, c(317L, 332L, 317L, 333L, 218L, 146L))
-# })
+
+test_that("Only accepts date objects", {
+  posix <- seq.POSIXt(as.POSIXct("2012-01-01"), as.POSIXct("2012-01-31"), by = "hour")
+  test <- data.frame(posix, val = rnorm(length(posix), 20, 5))
+
+  expect_error(pm_yearly_98(test, "posix", "val"))
+})
+
+test_that("Is a data frame", {
+  expect_is(test_one, "data.frame")
+  expect_is(test_mult, "data.frame")
+})
+
+ 
+test_that("Has the right column names and dimensions", {
+  expected_names <-c("year", "ann_98_percentile", "valid_in_year", "quarter_1", 
+                     "quarter_2", "quarter_3", "quarter_4", "ann_avg", "valid_year", 
+                     "exceed", "flag_year_based_on_incomplete_data")
+  expect_equal(names(test_one), expected_names)
+  expect_equal(dim(test_one), c(4, 11))
+
+  # For multiple years:
+  expect_equal(names(test_mult), c("id", expected_names))
+  expect_equal(dim(test_mult), c(8, 12))
+})
+ 
+test_that("Columns are the right class", {
+  classes <- c("integer", "numeric", "numeric", "numeric", "numeric", "numeric", 
+               "numeric", "numeric", "logical", "logical", "logical")
+  expect_equal(unname(sapply(test_one, class)), classes)
+  expect_equal(unname(sapply(test_mult, class)), c("character", classes))
+})
+ 
+test_that("Exceed works", {
+  expect_false(any(test_mult$exceed, na.rm = TRUE))
+
+  set.seed(42)
+  multi_id$val <- rnorm(nrow(multi_id), 35, 1)
+  res <- pm_yearly_98(multi_id, dt = "dates", val = "val", by = "id")
+  expect_true(all(res$exceed))
+})
+ 
+test_that("Number of valid days in year correct", {
+  expect_equal(c(365,365,366,365) * test_one$valid_in_year, c(1L, 361L, 358L, 357L))
+  expect_equal(c(365,365,366,365) * test_mult$valid_in_year, c(1L, 361L, 358L, 357L, 1L, 365L, 243L, 170L))
+})
