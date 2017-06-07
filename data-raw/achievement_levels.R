4b0226e421d70236e579f447a1eec9ce10d73f02
Andy Teucher <andy.teucher@gov.bc.ca>
2017-06-07 13:44:46 -0700

    Tidier construction of achievement_levels

--- data-raw/achievement_levels.R
+++ data-raw/achievement_levels.R
@@ -1,42 +1,28 @@
-parameter <- c(rep("o3", 2), rep("pm2.5_annual", 2), rep("pm2.5_24h", 2))
+library(tibble)
+library(dplyr)
 
-labels <- rep(c("Achieved", 
-                "Not Achieved"), 3)
+label_colours <- c("Achieved" = "#377EB8", "Not Achieved" = "#E41A1C")
 
-lower_breaks <- c(0, 63, 
-                  0, 10.0, 
-                  0, 28)
+units_tbl <- tibble(
+  units = c("ppb", "ug/m^3"),
+  units_unicode = c("ppb", "\u03BCg/m\u00B3"), 
+  units_html = c("ppb", "&mu;g/m&sup3;")
+)
 
-upper_breaks <- c(63, Inf,
-                  10.0, Inf,
-                  28, Inf)
-
-units_unicode <- c(rep("ppb", 2), 
-                   rep("\u03BCg/m\u00B3", 2), 
-                   rep("\u03BCg/m\u00B3", 2))
-
-units_html <- c(rep("ppb", 2), 
-                rep("&mu;g/m&sup3;", 2), 
-                rep("&mu;g/m&sup3;", 2))
-
-val_labels_unicode <- c(paste0("\u2264 ", lower_breaks[2], units_unicode[1]), 
-                        paste0("\u003E ", lower_breaks[2], units_unicode[1]), 
-                        paste0("\u2264 ", lower_breaks[4], units_unicode[3]), 
-                        paste0("\u003E ", lower_breaks[4], units_unicode[3]), 
-                        paste0("\u2264 ", lower_breaks[6], units_unicode[5]), 
-                        paste0("\u003E ", lower_breaks[6], units_unicode[5]))
-
-val_labels_html <- c(paste0("&leq; ", lower_breaks[2], units_html[1]), 
-                     paste0("&gt; ", lower_breaks[2], units_html[1]), 
-                     paste0("&leq; ", lower_breaks[4], units_html[3]), 
-                     paste0("&gt; ", lower_breaks[4], units_html[3]), 
-                     paste0("&leq; ", lower_breaks[6], units_html[5]), 
-                     paste0("&gt; ", lower_breaks[6], units_html[5]))
-
-colour <- rep(c("#377EB8", "#E41A1C"), 3)
-
-achievement_levels <- data.frame(parameter, labels, lower_breaks, upper_breaks, 
-                           units_html, units_unicode, val_labels_html, 
-                           val_labels_unicode, colour)
-
-
+achievement_levels <- tribble(
+  ~parameter,     ~labels,        ~lower_breaks, ~upper_breaks, ~units,
+  "o3",           "Achieved",     0,             63,            "ppb",
+  "o3",           "Not Achieved", 63,            Inf,           "ppb",
+  "pm2.5_annual", "Achieved",     0,             10.0,          "ug/m^3",
+  "pm2.5_annual", "Not Achieved", 10.0,          Inf,           "ug/m^3",
+  "pm2.5_24h",    "Achieved",     0,             28,            "ug/m^3",
+  "pm2.5_24h",    "Not Achieved", 28,            Inf,           "ug/m^3"
+) %>% 
+  left_join(units_tbl, by = "units") %>% 
+  mutate(val_labels_unicode = ifelse(is.infinite(upper_breaks), 
+                                     paste0("\u003E ", lower_breaks, units_unicode), 
+                                     paste0("\u2264 ", upper_breaks, units_unicode)), 
+         val_labels_html = ifelse(is.infinite(upper_breaks), 
+                                  paste0("&gt; ", lower_breaks, units_html), 
+                                  paste0("&leq; ", upper_breaks, units_html)), 
+         colour = label_colours[labels])
