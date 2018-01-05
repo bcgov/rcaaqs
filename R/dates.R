c1ee32e55077673ed5c5dc286763deaa4fcd11a1
Stefanie LaZerte <steffi@steffi.ca>
2018-01-05 11:45:02 -0600

    dates.R Format documentation, switch to explicit imports, don't export all functions

--- R/dates.R
+++ R/dates.R
@@ -10,30 +10,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
+
 #' Format date-times in raw air quality data.
 #' 
-#' Intended for use with hourly air quality readings. Ensures that the date/times are in class 
-#' POSIXct in the correct timezone, and ensures that the timestamp is associated with 
-#' the previous hour as dictated in caaqs guidance manual.
-#'
-#' @importFrom lubridate fast_strptime is.POSIXct tz
+#' Intended for use with hourly air quality readings. Ensures that the
+#' date/times are in class POSIXct in the correct timezone, and ensures that the
+#' timestamp is associated with the previous hour as dictated in caaqs guidance
+#' manual.
 #'
 #' @param  x vector of date-times as character or POSIXlt/ct.
 #' @param format the format of the character dates
-#' @param prev_hour should the timestamp be assigned to the previous hour as dictated
-#' by the CAAQS guidance document? Default \code{TRUE}. This is accomplished by subtracting one 
-#' second off the times.
+#' @param prev_hour should the timestamp be assigned to the previous hour as
+#'   dictated by the CAAQS guidance document? Default \code{TRUE}. This is
+#'   accomplished by subtracting one second off the times.
 #' @param tz the timezone of the date-times. See Details below.
 #' 
-#' @details You can set the timezone that you are working with in two different ways. You can set it 
-#' globally with: \code{options("rcaaqs.timezone" = "your_timezone")} or set it each time you call the 
-#' function by setting the \code{tz} argument to "your_timezone" where "your_timezone" is a valid 
-#' timezone - see \code{\link{OlsonNames}}. The default (when \code{options("rcaaqs.timezone")} is unset) 
-#' is \code{"Etc/GMT+8"}, which is equivalent to Pacific Standard Time, but does not use Daylight Savings 
-#' (i.e., is GMT+8 all year). This is the standard for Air Quality Monitoring in British Columbia.
+#' @details You can set the timezone that you are working with in two different
+#'   ways. You can set it globally with: \code{options("rcaaqs.timezone" =
+#'   "your_timezone")} or set it each time you call the function by setting the
+#'   \code{tz} argument to "your_timezone" where "your_timezone" is a valid 
+#'   timezone - see \code{\link{OlsonNames}}. The default (when
+#'   \code{options("rcaaqs.timezone")} is unset) is \code{"Etc/GMT+8"}, which is
+#'   equivalent to Pacific Standard Time, but does not use Daylight Savings 
+#'   (i.e., is GMT+8 all year). This is the standard for Air Quality Monitoring
+#'   in British Columbia.
+#'
+#' @return POSIXct vector
 #'
 #' @export
-#' @return POSIXct vector
+
 format_caaqs_dt <- function(x, format="%Y-%m-%d %H:%M:%S", prev_hour = TRUE,
                             tz = getOption("rcaaqs.timezone", default = "Etc/GMT+8")) {
   
@@ -62,10 +68,11 @@ format_caaqs_dt <- function(x, format="%Y-%m-%d %H:%M:%S", prev_hour = TRUE,
   
 }
 
-#'Fill gaps in a date sequence
+#' Fill gaps in a date sequence
 #'
-#'Given a dataframe with one column as a date sequence, fill gaps in the dat
-#'sequence.
+#' Given a dataframe with one column as a date sequence, fill gaps in the dat 
+#' sequence.
+#' 
 #' @param  df Dataframe
 #' @param  date_col the column containing dates
 #' @param  interval The interval in the date sequence. If \code{NULL}, calculated
@@ -74,14 +81,18 @@ format_caaqs_dt <- function(x, format="%Y-%m-%d %H:%M:%S", prev_hour = TRUE,
 #'  columns where value is same in every row, such as an ID.)
 #' @param  add_ymd logical. Should the date be split into year, month, and day
 #'  columns and added to the output?
-#' @export
-# '@return dataframe with filled in dates
+#'  
+#' @return dataframe with filled in dates
+#' 
 #' @examples \dontrun{
 #' foo <- data.frame(Date = seq(as.Date("2008-01-01"), as.Date("2008-12-01"), by = "month"), 
 #'                   val = round(rnorm(12, 5, 2), 1), label = rep("a", 12))
 #' bar <- foo[-c(2,5,6,7,10),]
 #' date_fill(bar, "Date", interval = "1 month", fill_cols = "label")
-#'}
+#' }
+#' 
+#' @noRd
+
 date_fill <- function(df, date_col, interval = NULL, fill_cols = NULL, add_ymd = FALSE) {
   
   if (!is.null(interval) && 
@@ -129,15 +140,17 @@ date_fill <- function(df, date_col, interval = NULL, fill_cols = NULL, add_ymd =
   out
 }
 
-#'Add a year, month, and day column to a dataframe based on a date column
+#' Add a year, month, and day column to a dataframe based on a date column
 #'
 #' @param df dataframe
 #' @param datecol the column in df that contains the dates as a character string
 #' @param outnames the names for the new columns in order of year, month, day 
-#'                 (defaults to "year", "month", "day")
+#'   (defaults to "year", "month", "day")
 #' @param tz the timezone
-#' @export
+#' 
 #' @return a dataframe with the new columns added
+#' 
+#' @noRd
 
 add_ymd <- function(df, datecol, tz = getOption("rcaaqs.timezone", default = "Etc/GMT+8"), 
                     outnames = NULL) {
@@ -170,26 +183,33 @@ add_ymd <- function(df, datecol, tz = getOption("rcaaqs.timezone", default = "Et
 days_in_quarter <- function(quarter, year) 
   c(90,91,92,92)[quarter] + (leap_year(year) & quarter == 1)
 
-#'Calcuate number of days in year
+#' Calcuate number of days in year
 #'
-#' @importFrom lubridate leap_year
 #' @param year the year as a numeric
+#' 
 #' @return vector with the number of days in that year.
-days_in_year <- function(year) 
+#' 
+#' @noRd
+days_in_year <- function(year) {
   365 + lubridate::leap_year(year)
+}
 
-#'Interpolate a sequence of dates and values with NA when dates are missing.
+#' Interpolate a sequence of dates and values with NA when dates are missing
 #'
-#' @importFrom tidyr full_seq
 #' @param dat the date, date_time, or numeric value
 #' @param val some value to be interpolated
-#' @param interval some numeric step size. for date-times, in seconds.
+#' @param interval some numeric step size. for date-times, in seconds
+#' 
 #' @return data.frame with all input dates and values, interpolated with NAs.
+#' 
+#' @noRd
+
 pad_date_time <- function(dat, val, interval) {
-  all_dates <- full_seq(dat, interval)
+  all_dates <- tidyr::full_seq(dat, interval)
   data.frame(dat = all_dates, 
              val = val[match(all_dates, dat)])
 }
+
 get_year_from_date <- function(date) {
   as.integer(as.POSIXlt(date)$year + 1900)
 }
@@ -204,44 +224,48 @@ get_day_from_date <- function(date) {
 
 # Simply using as.Date on a time converts based on UTC.
 # This conversion just truncates the time part.
-time_to_date <- function(date_time) 
+time_to_date <- function(date_time) {
   as.Date(date_time, attr(date_time, "tzone"))
+}
 
-#'Calcuate number (or proportion) of valid days in each year and quarter
+#' Calcuate number (or proportion) of valid days in each year and quarter
 #'
-#' @importFrom lubridate quarter
-#' @importFrom tidyr spread_
 #' @param data the input data.frame
 #' @param date the date
 #' @param by the grouping variable
-#' @param units either "prop" if you want proportion of valid days or "days" is absolute number of days is required.
+#' @param units either "prop" if you want proportion of valid days or "days" is
+#'   absolute number of days is required.
+#'   
 #' @return data.frame with valid days in each year and quarter
+#' 
+#' @noRd
+
 valid_by_quarter <- function(data, date, by, units = c("prop", "days")) {
-  data <- mutate_(data, 
-                  year    = interp(~get_year_from_date(date), date = as.name(date)), 
-                  quarter = interp(~quarter(date), date = as.name(date)))
-  data <- group_by_(data, .dots = c(by, "year", "quarter"))
-  data <- summarise(data, days = n())
-  data <- ungroup(data)
+  data <- dplyr::mutate_(data, 
+                         year    = lazyeval::interp(~get_year_from_date(date), date = as.name(date)), 
+                         quarter = lazyeval::interp(~lubridate::quarter(date), date = as.name(date)))
+  data <- dplyr::group_by_(data, .dots = c(by, "year", "quarter"))
+  data <- dplyr::summarise(data, days = n())
+  data <- dplyr::ungroup(data)
   
   # Cheap way to fill in quarters.  
   all_q = do.call(rbind, replicate(4, unique(data[c(by, "year")]), simplify = FALSE))
   all_q$quarter <- rep(1:4, each = nrow(all_q)/4)
-  data <- left_join(all_q, data, by = c(by, "year", "quarter"))
+  data <- dplyr::left_join(all_q, data, by = c(by, "year", "quarter"))
   data$days <- ifelse(is.na(data$days), 0, data$days)
   data$valid_in_quarter <- data$days
   if(units == "prop") 
     data$valid_in_quarter <- data$valid_in_quarter / days_in_quarter(data$quarter, data$year)
-  data <- group_by_(data, .dots = c(by, "year"))
-  data <- mutate_(data, valid_in_year = ~sum(days))
+  data <- dplyr::group_by_(data, .dots = c(by, "year"))
+  data <- dplyr::mutate_(data, valid_in_year = ~sum(days))
   if(units == "prop") 
     data$valid_in_year <- data$valid_in_year / days_in_year(data$year)
-  data <- arrange_(data, .dots = c(by, "year", "quarter"))
+  data <- dplyr::arrange_(data, .dots = c(by, "year", "quarter"))
   
   # Format to wide columns.  
-  data <- group_by_(data, .dots = c("year", by))
-  data <- select_(data, .dots = c(by, "year", "quarter", "valid_in_quarter", "valid_in_year"))
-  data <- spread_(data, "quarter", "valid_in_quarter", sep = "_")
+  data <- dplyr::group_by_(data, .dots = c("year", by))
+  data <- dplyr::select_(data, .dots = c(by, "year", "quarter", "valid_in_quarter", "valid_in_year"))
+  data <- tidyr::spread_(data, "quarter", "valid_in_quarter", sep = "_")
   
   ungroup(data)
 }
