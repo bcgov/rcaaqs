67780429c4ebba8514e4c64ac9e72e720c73305f
Stefanie LaZerte <steffi@steffi.ca>
2018-01-05 11:47:04 -0600

    clean_neg.R Format documentation, don't export all functions

--- R/clean_neg.R
+++ R/clean_neg.R
@@ -10,19 +10,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#'Replaces negative values in a vector of air quality readings with
-#'CAAQS-compliant values
+
+#' Replaces negative values in a vector of air quality readings with 
+#' CAAQS-compliant values
 #'
-#'@param x vector of air quality readings
-#'@param  type type of air quality readings; one of: "pm25", "ozone", "so2",
-#'  "nox"
-#'@export
-#'@return a vector of the same length as x with negative values replaced with
-#'  CAAQS compliant values
-#'@details For gases (type = "ozone", "so2", or "nox"), replaces values
-#'  \eqn{\le} -5 with \code{NA} and values between -5 and 0 with 0. For type =
-#'  "pm25", replaces values \eqn{\le} -3.5 with \code{NA} and values between
-#'  -3.5 and 0 with 0.
+#' @param x vector of air quality readings
+#' @param  type type of air quality readings; one of: "pm25", "ozone", "so2", 
+#'   "nox"
+#'  
+#' @details For gases (type = "ozone", "so2", or "nox"), replaces values 
+#'   \eqn{\le} -5 with \code{NA} and values between -5 and 0 with 0. For type = 
+#'   "pm25", replaces values \eqn{\le} -3.5 with \code{NA} and values between 
+#'   -3.5 and 0 with 0.
+#'  
+#' @return a vector of the same length as x with negative values replaced with 
+#'   CAAQS compliant values
+#'
+#' @noRd
 
 clean_neg <- function(x, type = c("pm25", "ozone", "so2", "nox")) {
   type <- tolower(type)
