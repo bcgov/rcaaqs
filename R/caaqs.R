bf5c648cc5fc953a8d1542c36a5441db11e41fde
Stefanie LaZerte <steffi@steffi.ca>
2018-01-05 11:18:56 -0600

    Create generic caaq metric function

--- R/caaqs.R
+++ R/caaqs.R
@@ -1,3 +1,74 @@
+#' @importFrom rlang .data
+caaq <- function(df, year, val, by, metric, n) {
+
+  # Check inputs
+  check_vars(list(year, val, by), df)
+  check_mode(year, df, "numeric")
+  check_mode(val, df, "numeric")
+  
+  # Rename to standard
+  names(df)[names(df) == year] <- "year"
+  names(df)[names(df) == val] <- "metric_value"
+  
+  # Start with ungrouped df
+  df <- dplyr::ungroup(df)
+  
+  # Group df if supplied
+  if(!is.null(by)) {
+    if(n == 1) message("Grouping doesn't apply to single year caaqs. 'by' ignored.")
+    if(n == 3) df <- dplyr::group_by_if(df, names(df) %in% by)
+  }
+  
+  # Order by date (within grouping if applied)
+  df <- dplyr::arrange(df, !!! rlang::syms(c(by, "year")))
+  
+  if(n == 1) {
+    # Round caaqs metric (already completed for 3yr, but necessary for 1yr)
+    df$metric_value <- round_caaqs(df$metric_value)
+  }
+  if(n == 3) {
+    
+    # Check for duplicates (considering grouping)
+    dup <- dplyr::group_by(df, .data$year, add = TRUE)
+    dup <- dplyr::summarize(dup, n = length(.data$year))
+    if(any(dup$n > 1)) {
+      msg <- paste0("Duplicate values in '", year, "'.")
+      if(is.null(by)) msg <- paste0(msg, " Consider using 'by' to specify grouping argument(s).")
+      if(!is.null(by)) msg <- paste0(msg, " Consider adding additional grouping variables to 'by'.")
+      stop(msg)
+    }
+
+    # Calculate min/max and total number of years in rolling averages
+    df <- dplyr::mutate(df, 
+                        year_lag1 = dplyr::lag(.data$year),
+                        year_lag2 = dplyr::lag(.data$year, 2),
+                        min_year = dplyr::case_when(!is.na(.data$year_lag2) ~ .data$year_lag2,
+                                                    !is.na(.data$year_lag1) ~ .data$year_lag1,
+                                                    TRUE ~ .data$year),
+                        max_year = .data$year,
+                        n_max = as.integer(max(.data$n_years, na.rm = TRUE)))
+    
+    # Extract 3-year average if 3 or more years in the data, otherwise, use 2-year average
+    df <- dplyr::filter(df, .data$n_years == .data$n_max)
+    
+    df <- dplyr::ungroup(df)
+  }
+
+  # Consider flagging data based on incomplete?
+  
+  ## Determine station achievements
+  df$caaqs <- cut_achievement(df$metric_value, metric, output = "labels")
+  df$mgmt <- cut_management(df$metric_value, metric)
+  df$metric = metric
+  df$caaq_year <- df$year
+  
+  # Clean up
+  df <- dplyr::select(df, dplyr::one_of(by, "caaq_year", "min_year", 
+                                        "max_year", "n_years", "metric", 
+                                        "metric_value", "caaqs", "mgmt"))
+  
+  df
+}
 
 #' Calculate the three-year average for the Ozone CAAQ metric
 #' 
@@ -29,89 +100,27 @@
 #' 
 #' @export
 o3_caaq <- function(df, year = "year", val = "ozone_metric", by = NULL) {
-
-  check_vars(list(year, val, by), df)
-  
-  # Rename to standard
-  names(df)[names(df) == year] <- "year"
-  names(df)[names(df) == val] <- "ozone_metric"
-  
-  # Start with ungrouped df
-  df <- dplyr::ungroup(df)
-  
-  # Group df if supplied
-  if(!is.null(by)) df <- dplyr::group_by_if(df, names(df) %in% by)
-  
-  # Check for duplicates (considering grouping)
-  dup <- dplyr::group_by(df, .data$year, add = TRUE)
-  dup <- dplyr::summarize(dup, n = length(.data$year))
-  if(any(dup$n > 1)) {
-    msg <- paste0("Duplicate values in '", year, "'.")
-    if(is.null(by)) msg <- paste0(msg, " Consider using 'by' to specify grouping argument(s).")
-    if(!is.null(by)) msg <- paste0(msg, " Consider adding additional grouping variables to 'by'.")
-    stop(msg)
-  }
-  
-  # Order by date (within grouping if applied)
-  df <- dplyr::arrange(df, !!!lapply(c(by, "year"), as.name))
-  
-  # Calculate number of years in rolling averages
-  df <- dplyr::mutate(df, 
-                      year_lag1 = dplyr::lag(.data$year),
-                      year_lag2 = dplyr::lag(.data$year, 2),
-                      n_total = length(.data$year),
-                      min_year = dplyr::case_when(!is.na(.data$year_lag2) ~ .data$year_lag2,
-                                                  !is.na(.data$year_lag1) ~ .data$year_lag1,
-                                                  TRUE ~ .data$year),
-                      max_year = .data$year,
-                      n_years = .data$max_year - .data$min_year + 1L)
-  
-  # Extract 3-year average if 3 or more years in the data, otherwise, use 2-year average
-  df <- dplyr::filter(df, (.data$n_years == 3 & .data$n_total >= 3) | 
-                          (.data$n_years == 2 & .data$n_total == 2))
-  
-  df <- dplyr::ungroup(df)
-  
-  # Round ozone caaqs metric
-  df$metric_value <- round_caaqs(df$ozone_metric)
-  
-  # Consider flagging data based on incomplete?
-  
-  ## Determine station achievements
-  df$caaqs <- cut_achievement(df$metric_value, "o3", output = "labels")
-  df$mgmt <- cut_management(df$metric_value, "o3")
-  df$metric = "ozone"
-  df$caaq_year <- df$year
-  
-  # Clean up
-  df <- dplyr::select(df, dplyr::one_of(by, "caaq_year", "min_year", 
-                                            "max_year", "n_years", "metric", 
-                                            "metric_value", "caaqs", "mgmt"))
-  
-  df
+  caaq(df, year, val, by, metric = "o3", n = 3)
 }
 
-
-so2_1yr_caaq <- function() {
+#' @export
+so2_1yr_caaq <- function(df, year = "year", val = "ann_99_percentile", by = NULL) {
   
-  
-  
+  # CHECK VALIDITY
+  caaq(df, year, val, by, metric = "so2_1yr", n = 1)
 }
 
-so2_3yr_caaq <- function() {
-  
-  
-  
+#' @export
+so2_3yr_caaq <- function(df, year = "year", val = "so2_metric", by = NULL) {
+  caaq(df, year, val, by, metric = "so2_3yr", n = 3)
 }
 
-no2_1yr_caaq <- function() {
-  
-  
-  
+#' @export
+no2_1yr_caaq <- function(df, year = "year", val = "avg_yearly", by = NULL) {
+  caaq(df, year, val, by, metric = "no2_1yr", n = 1)
 }
 
-no2_3yr_caaq <- function() {
-  
-  
-  
+#' @export
+no2_3yr_caaq <- function(df, year = "year", val = "no2_metric", by = NULL) {
+  caaq(df, year, val, by, metric = "no2_3yr", n = 3)
 }
