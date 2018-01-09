4df8ec4874e78776beef13e2fbed781aa7d56038
Stefanie LaZerte <steffi@steffi.ca>
2018-01-09 13:26:39 -0600

    Update tests to match new airzone_metric() arguments

--- tests/testthat/test-airzone_metric.R
+++ tests/testthat/test-airzone_metric.R
@@ -16,7 +16,7 @@ test_that("airzone_metric works", {
   df <- data.frame(Airzone = sort(rep(LETTERS[1:4],4)), 
                    nyears = c(rep(2, 4), rep(3, 4), c(2,3,2,3,2,3,2,3)), 
                    value = c(1:4, 5:8, 12:9, 13:16), stringsAsFactors = FALSE)
-  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", val = "value")
+  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", caaq = "value")
   expect_equal(dim(res), c(4,3))
   expect_equal(res$Airzone, LETTERS[1:4])
   expect_equal(res$nyears, c(2,3,3,3))
@@ -28,7 +28,7 @@ test_that("keep arg works", {
                    nyears = c(rep(2, 4), rep(3, 4), c(2,3,2,3,2,3,2,3)), 
                    value = c(1:4, 5:8, 12:9, 13:16), 
                    otherdata = rep(c("foo", "bar"), 8), stringsAsFactors = FALSE)
-  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", val = "value", 
+  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", caaq = "value",
                         keep = c("site_id", "otherdata"))
   expect_equal(dim(res), c(4,5))
   expect_equal(res$Airzone, LETTERS[1:4])
@@ -42,10 +42,10 @@ test_that("renaming keep cols works", {
                    nyears = c(rep(2, 4), rep(3, 4), c(2,3,2,3,2,3,2,3)), 
                    value = c(1:4, 5:8, 12:9, 13:16), 
                    otherdata = rep(c("foo", "bar"), 8), stringsAsFactors = FALSE)
-  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", val = "value", 
+  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", caaq = "value",
                         keep = c(rep_site = "site_id", foobar = "otherdata"))
   expect_equal(names(res), c("Airzone", "nyears", "value", "rep_site", "foobar"))
-  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", val = "value", 
+  res <- airzone_metric(df, n_years = "nyears", az = "Airzone", caaq = "value",
                         keep = c(rep_site = "site_id", "otherdata", yearssss = "nyears"))
   expect_equal(names(res), c("Airzone", "yearssss", "value", "rep_site", "otherdata"))
 })
