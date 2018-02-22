fccd3db4a740f178961f39d874f2dd00ec317a5a
Stefanie LaZerte <steffi@steffi.ca>
2018-02-22 11:20:24 -0600

    Don't test airzones on cran, travis, or appveyor

--- tests/testthat/test-8_airzone_metric.R
+++ tests/testthat/test-8_airzone_metric.R
@@ -77,6 +77,10 @@ test_that("assign_airzone catches incorrect input", {
 })
 
 test_that("assign_airzone returns expected input", {
+  skip_on_appveyor()
+  skip_on_cran()
+  skip_on_travis()
+  
   p <- readRDS("pm_24h_caaq2.rds") %>%
     dplyr::mutate(latitude = c(rep(49.9, 3), rep(48.4, 3)),
                   longitude = c(rep(-119, 3), rep(-123, 3)))
