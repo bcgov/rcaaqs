ea4d7473ec7d60676ded6e3d5e4421b959aea5bd
Andy Teucher <andy.teucher@gov.bc.ca>
2018-11-16 16:25:30 -0800

    o3 tests

--- tests/testthat/test-7_o3_caaqs.R
+++ tests/testthat/test-7_o3_caaqs.R
@@ -1,56 +1,46 @@
 context("o3 caaqs wrappers")
 
+test_that("classes and extractors for o3", {
+  d <- readRDS("o3_raw1.rds")
+  o3_caaqs <- o3_caaqs(d)
+  expect_is(o3_caaqs, c("caaqs", "o3"))
+  expect_length(o3_caaqs, 4)
+  expect_named(o3_caaqs, c("daily_max", "ann_4th_highest", "three_yr_rolling", "caaqs"))
+  for (d in o3_caaqs) {
+    expect_is(d, "data.frame")
+  }
+  expect_is(extract_caaqs(o3_caaqs), "data.frame")
+  expect_is(extract_daily(o3_caaqs), "data.frame")
+  expect_is(extract_yearly(o3_caaqs), "data.frame")
+  expect_is(extract_three_yr_rolling(o3_caaqs), "data.frame")
+})
 
 test_that("o3_caaqs single", {
   d <- readRDS("o3_raw1.rds")
 
-  expect_error(expect_message(caaqs <- o3_caaqs(d), 
+  expect_error(expect_message(o3_caaqs <- o3_caaqs(d), 
                               "Calculating O3 daily maximum"),
                NA)
+  caaqs <- extract_caaqs(o3_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("o3")] 
                   == "Achieved", na.rm = TRUE))  
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("o3")] 
                   == "Not Achieved", na.rm = TRUE))
-  saveRDS(caaqs, "o3_caaqs1.rds")
 })
 
 test_that("o3_caaqs groups", {
   d <- readRDS("o3_raw2.rds")
   
-  expect_error(expect_message(caaqs <- o3_caaqs(d, by = c("ems_id", "site")), 
+  expect_error(expect_message(o3_caaqs <- o3_caaqs(d, by = c("ems_id", "site")), 
                               "Calculating O3 daily maximum"), NA)
   
+  caaqs <- extract_caaqs(o3_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("o3")] 
                   == "Achieved", na.rm = TRUE))
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("o3")] 
                   == "Not Achieved", na.rm = TRUE))  
-  saveRDS(caaqs, "o3_caaqs2.rds")
-})
-
-test_that("o3_caaqs single returns all", {
-  d <- readRDS("o3_raw1.rds")
-  
-  expect_error(expect_message(caaqs <- o3_caaqs(d, return_all = TRUE), 
-                              "Calculating O3 daily maximum"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 5)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("o3_caaqs1.rds"))
-})
-
-test_that("o3_caaqs group returns all", {
-  d <- readRDS("o3_raw2.rds")
-  
-  expect_error(expect_message(caaqs <- o3_caaqs(d, 
-                                              by = c("ems_id", "site"),
-                                              return_all = TRUE), 
-                              "Calculating O3 daily maximum"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 7)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("o3_caaqs2.rds"))
 })
