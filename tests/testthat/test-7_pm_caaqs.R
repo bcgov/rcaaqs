4a4b0ef305d0dc80f069ab59fa6f45ab9aa1dacc
Andy Teucher <andy.teucher@gov.bc.ca>
2018-11-19 12:17:39 -0800

    pm and so2 tests

--- tests/testthat/test-7_pm_caaqs.R
+++ tests/testthat/test-7_pm_caaqs.R
@@ -1,12 +1,42 @@
 context("pm caaqs wrappers")
 
+test_that("classes and extractors for pm2.5 24hr", {
+  d <- readRDS("pm_raw1.rds")
+  pm_caaqs <- pm_24h_caaqs(d)
+  expect_is(pm_caaqs, c("caaqs", "pm_24h"))
+  expect_length(pm_caaqs, 4)
+  expect_named(pm_caaqs, c("daily_avg", "yearly_98", "three_yr_rolling", "caaqs"))
+  for (d in pm_caaqs) {
+    expect_is(d, "data.frame")
+  }
+  expect_is(extract_caaqs(pm_caaqs), "data.frame")
+  expect_is(extract_daily(pm_caaqs), "data.frame")
+  expect_is(extract_yearly(pm_caaqs), "data.frame")
+  expect_is(extract_three_yr_rolling(pm_caaqs), "data.frame")
+})
+
+test_that("classes and extractors for pm2.5 annual", {
+  d <- readRDS("pm_raw1.rds")
+  pm_caaqs <- pm_annual_caaqs(d)
+  expect_is(pm_caaqs, c("caaqs", "pm_annual"))
+  expect_length(pm_caaqs, 4)
+  expect_named(pm_caaqs, c("daily_avg", "yearly_avg", "three_yr_rolling", "caaqs"))
+  for (d in pm_caaqs) {
+    expect_is(d, "data.frame")
+  }
+  expect_is(extract_caaqs(pm_caaqs), "data.frame")
+  expect_is(extract_daily(pm_caaqs), "data.frame")
+  expect_is(extract_yearly(pm_caaqs), "data.frame")
+  expect_is(extract_three_yr_rolling(pm_caaqs), "data.frame")
+})
 
 test_that("pm_24h_caaqs single", {
   d <- readRDS("pm_raw1.rds")
 
-  expect_error(expect_message(caaqs <- pm_24h_caaqs(d), 
+  expect_error(expect_message(pm_caaqs <- pm_24h_caaqs(d), 
                               "Calculating PM 2.5 daily average"),
                NA)
+  caaqs <- extract_caaqs(pm_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("pm2.5_24h")] 
@@ -19,9 +49,9 @@ test_that("pm_24h_caaqs single", {
 test_that("pm_24h_caaqs groups", {
   d <- readRDS("pm_raw2.rds")
   
-  expect_error(expect_message(caaqs <- pm_24h_caaqs(d, by = c("ems_id", "site")), 
+  expect_error(expect_message(pm_caaqs <- pm_24h_caaqs(d, by = c("ems_id", "site")), 
                               "Calculating PM 2.5 daily average"), NA)
-  
+  caaqs <- extract_caaqs(pm_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("pm2.5_24h")] 
@@ -31,36 +61,13 @@ test_that("pm_24h_caaqs groups", {
   saveRDS(caaqs, "pm_24h_caaqs2.rds")
 })
 
-test_that("pm_24h_caaqs single returns all", {
-  d <- readRDS("pm_raw1.rds")
-  
-  expect_error(expect_message(caaqs <- pm_24h_caaqs(d, return_all = TRUE), 
-                              "Calculating PM 2.5 daily average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 4)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("pm_24h_caaqs1.rds"))
-})
-
-test_that("pm_24h_caaqs group returns all", {
-  d <- readRDS("pm_raw2.rds")
-  
-  expect_error(expect_message(caaqs <- pm_24h_caaqs(d, 
-                                                  by = c("ems_id", "site"),
-                                                  return_all = TRUE), 
-                              "Calculating PM 2.5 daily average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 6)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("pm_24h_caaqs2.rds"))
-})
-
 test_that("pm_annual_caaqs single", {
   d <- readRDS("pm_raw1.rds")
   
-  expect_error(expect_message(caaqs <- pm_annual_caaqs(d), 
+  expect_error(expect_message(pm_caaqs <- pm_annual_caaqs(d), 
                               "Calculating PM 2.5 daily average"),
                NA)
+  caaqs <- extract_caaqs(pm_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("pm2.5_annual")] 
@@ -73,9 +80,9 @@ test_that("pm_annual_caaqs single", {
 test_that("pm_annual_caaqs groups", {
   d <- readRDS("pm_raw2.rds")
   
-  expect_error(expect_message(caaqs <- pm_annual_caaqs(d, by = c("ems_id", "site")), 
+  expect_error(expect_message(pm_caaqs <- pm_annual_caaqs(d, by = c("ems_id", "site")), 
                               "Calculating PM 2.5 daily average"), NA)
-  
+  caaqs <- extract_caaqs(pm_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("pm2.5_annual")] 
@@ -83,28 +90,4 @@ test_that("pm_annual_caaqs groups", {
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("pm2.5_annual")] 
                   == "Not Achieved", na.rm = TRUE))  
   saveRDS(caaqs, "pm_annual_caaqs2.rds")
-})
-
-test_that("pm_annaul_caaqs single returns all", {
-  d <- readRDS("pm_raw1.rds")
-  
-  expect_error(expect_message(caaqs <- pm_annual_caaqs(d, return_all = TRUE), 
-                              "Calculating PM 2.5 daily average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 4)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("pm_annual_caaqs1.rds"))
-})
-
-test_that("pm_annual_caaqs group returns all", {
-  d <- readRDS("pm_raw2.rds")
-  
-  expect_error(expect_message(caaqs <- pm_annual_caaqs(d, 
-                                                  by = c("ems_id", "site"),
-                                                  return_all = TRUE), 
-                              "Calculating PM 2.5 daily average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 6)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("pm_annual_caaqs2.rds"))
 })
--- tests/testthat/test-7_so2_caaqs.R
+++ tests/testthat/test-7_so2_caaqs.R
@@ -1,114 +1,91 @@
 context("so2 caaqs wrappers")
 
+test_that("classes and extractors for so2 3yr", {
+  d <- readRDS("so2_raw1.rds")
+  so2_caaqs <- so2_3yr_caaqs(d)
+  expect_is(so2_caaqs, c("caaqs", "so2_3yr"))
+  expect_length(so2_caaqs, 4)
+  expect_named(so2_caaqs, c("daily_max", "yearly_99", "three_yr_rolling", "caaqs"))
+  for (d in so2_caaqs) {
+    expect_is(d, "data.frame")
+  }
+  expect_is(extract_caaqs(so2_caaqs), "data.frame")
+  expect_is(extract_daily(so2_caaqs), "data.frame")
+  expect_is(extract_yearly(so2_caaqs), "data.frame")
+  expect_is(extract_three_yr_rolling(so2_caaqs), "data.frame")
+})
+
+test_that("classes and extractors for so2 1yr", {
+  d <- readRDS("so2_raw1.rds")
+  so2_caaqs <- so2_1yr_caaqs(d)
+  expect_is(so2_caaqs, c("caaqs", "so2_1yr"))
+  expect_length(so2_caaqs, 2)
+  expect_named(so2_caaqs, c("yearly_hr", "caaqs"))
+  for (d in so2_caaqs) {
+    expect_is(d, "data.frame")
+  }
+  expect_is(extract_caaqs(so2_caaqs), "data.frame")
+  expect_error(extract_daily(so2_caaqs))
+  expect_is(extract_yearly(so2_caaqs), "data.frame")
+  expect_error(extract_three_yr_rolling(so2_caaqs))
+})
+
 
 test_that("so2_3yr_caaqs single", {
   d <- readRDS("so2_raw1.rds")
 
-  expect_error(expect_message(caaqs <- so2_3yr_caaqs(d), 
+  expect_error(expect_message(so2_caaqs <- so2_3yr_caaqs(d), 
                               "Calculating SO2 daily maximum"),
                NA)
+  caaqs <- extract_caaqs(so2_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("so2_3yr")] 
                   == "Achieved", na.rm = TRUE))  
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("so2_3yr")] 
                   == "Not Achieved", na.rm = TRUE))  
-  saveRDS(caaqs, "so2_3yr_caaqs1.rds")
 })
 
 test_that("so2_3yr_caaqs groups", {
   d <- readRDS("so2_raw2.rds")
   
-  expect_error(expect_message(caaqs <- so2_3yr_caaqs(d, by = c("ems_id", "site")), 
+  expect_error(expect_message(so2_caaqs <- so2_3yr_caaqs(d, by = c("ems_id", "site")), 
                  "Calculating SO2 daily maximum"), NA)
-  
+  caaqs <- extract_caaqs(so2_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("so2_3yr")] 
                   == "Achieved", na.rm = TRUE))
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("so2_3yr")] 
                   == "Not Achieved", na.rm = TRUE))  
-  saveRDS(caaqs, "so2_3yr_caaqs2.rds")
+
 })
-
-
-test_that("so2_3yr_caaqs single returns all", {
-  d <- readRDS("so2_raw1.rds")
-  
-  expect_error(expect_message(caaqs <- so2_3yr_caaqs(d, return_all = TRUE), 
-                              "Calculating SO2 daily maximum"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 4)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("so2_3yr_caaqs1.rds"))
-})
-
-test_that("so2_3yr_caaqs group returns all", {
-  d <- readRDS("so2_raw2.rds")
-  
-  expect_error(expect_message(caaqs <- so2_3yr_caaqs(d, 
-                                              by = c("ems_id", "site"),
-                                              return_all = TRUE), 
-                              "Calculating SO2 daily maximum"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 6)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("so2_3yr_caaqs2.rds"))
-})
-
-
-
 
 test_that("so2_1yr_caaqs single", {
   d <- readRDS("so2_raw1.rds")
   
-  expect_error(expect_message(caaqs <- so2_1yr_caaqs(d), 
+  expect_error(expect_message(so2_caaqs <- so2_1yr_caaqs(d), 
                               "Calculating SO2 annual average"),
                NA)
+  caaqs <- extract_caaqs(so2_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("so2_1yr")] 
                   == "Achieved", na.rm = TRUE))  
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("so2_1yr")] 
                   == "Not Achieved", na.rm = TRUE))  
-  saveRDS(caaqs, "so2_1yr_caaqs1.rds")
 })
 
 test_that("so2_1yr_caaqs groups", {
   d <- readRDS("so2_raw2.rds")
   
-  expect_error(expect_message(caaqs <- so2_1yr_caaqs(d, by = c("ems_id", "site")), 
+  expect_error(expect_message(so2_caaqs <- so2_1yr_caaqs(d, by = c("ems_id", "site")), 
                               "Calculating SO2 annual average"), NA)
-  
+  caaqs <- extract_caaqs(so2_caaqs)
   expect_true(all(is.na(caaqs$metric_value[caaqs$caaqs == "Insufficient Data"])))
   expect_true(all(!is.na(caaqs$metric_value[caaqs$caaqs != "Insufficient Data"])))
   expect_true(all(caaqs$caaqs[caaqs$metric_value <= get_std("so2_1yr")] 
                   == "Achieved", na.rm = TRUE))
   expect_true(all(caaqs$caaqs[caaqs$metric_value > get_std("so2_1yr")] 
-                  == "Not Achieved", na.rm = TRUE))  
-  saveRDS(caaqs, "so2_1yr_caaqs2.rds")
-})
-
-test_that("so2_1yr_caaqs single returns all", {
-  d <- readRDS("so2_raw1.rds")
-  
-  expect_error(expect_message(caaqs <- so2_1yr_caaqs(d, return_all = TRUE), 
-                              "Calculating SO2 annual average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 2)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("so2_1yr_caaqs1.rds"))
-})
-
-test_that("so2_1yr_caaqs group returns all", {
-  d <- readRDS("so2_raw2.rds")
-  
-  expect_error(expect_message(caaqs <- so2_1yr_caaqs(d, 
-                                                   by = c("ems_id", "site"),
-                                                   return_all = TRUE), 
-                              "Calculating SO2 annual average"),
-               NA)
-  expect_is(caaqs, "data.frame")
-  expect_length(caaqs, 4)
-  expect_equivalent(tidyr::unnest(caaqs, caaqs), readRDS("so2_1yr_caaqs2.rds"))
+                  == "Not Achieved", na.rm = TRUE))
 })
