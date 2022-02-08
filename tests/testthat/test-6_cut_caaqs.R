e8c0140fad93ea8aa5903535bcef37e25c0daa6e
steffilazerte <steffi@steffi.ca>
2022-02-08 15:31:49 -0600

    Update management levels too

--- tests/testthat/test-6_cut_caaqs.R
+++ tests/testthat/test-6_cut_caaqs.R
@@ -1,6 +1,6 @@
 context("cut breaks")
 
-o3_vec <- c(0, 40, 50, 50.01, 56, 56.01, 63, 63.01, 80, NA)
+o3_vec <- c(0, 40, 50, 50.01, 56, 56.01, 62, 62.01, 80, NA)
 
 test_that("cut_management returns a factor", {
   res <- cut_management(o3_vec, "o3")
@@ -23,8 +23,8 @@ test_that("management breaks are correct for o3: category", {
 test_that("management breaks are correct for o3: value labels (html)", {
   expected <- c(rep("&lteq; 50ppb", 3), 
                 rep("&gt; 50ppb &amp; &lteq; 56ppb", 2), 
-                rep("&gt; 56ppb &amp; &lteq; 63ppb", 2), 
-                rep("&gt; 63ppb", 2))
+                rep("&gt; 56ppb &amp; &lteq; 62ppb", 2), 
+                rep("&gt; 62ppb", 2))
   
   res <- cut_management(o3_vec, "o3", output = "breaks_h", drop_na = TRUE)
   expect_equal(as.character(res), c(expected, NA))
@@ -36,8 +36,8 @@ test_that("management breaks are correct for o3: value labels (html)", {
 test_that("management breaks are correct for o3: value labels (unicode)", {
   expected <- c(rep("\u2264 50ppb", 3), 
                 rep("\u003E 50ppb \u0026 \u2264 56ppb", 2), 
-                rep("\u003E 56ppb \u0026 \u2264 63ppb", 2), 
-                rep("\u003E 63ppb", 2))
+                rep("\u003E 56ppb \u0026 \u2264 62ppb", 2), 
+                rep("\u003E 62ppb", 2))
   
   res <- cut_management(o3_vec, "o3", output = "breaks_u", drop_na = TRUE)
   expect_equal(as.character(res), c(expected, NA))
